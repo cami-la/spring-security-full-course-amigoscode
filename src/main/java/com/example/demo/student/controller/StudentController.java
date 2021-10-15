ddb8de038331e9ec87c4c464bf8cf816e7ca4888
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-10-14 21:28:56 -0300

    util 20:16 - Form based Authentication Overview

---
+++ src/main/java/com/example/demo/student/controller/StudentController.java
@@ -0,0 +1,29 @@
+package com.example.demo.student.controller;
+
+import com.example.demo.student.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1l, "James Bond"),
+            new Student(2l, "Maria Jones"),
+            new Student(3l, "Anna Smith")
+    );
+
+    @GetMapping(path = "/{studentId}")
+    public Student getStudent(@PathVariable("studentId") Long studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format("Student %f does not exits.", studentId)));
+    }
+}
---
+++ src/main/java/com/example/demo/student/model/Student.java
@@ -0,0 +1,16 @@
+package com.example.demo.student.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    private Long studentId;
+    private String studentName;
+
+
+}
